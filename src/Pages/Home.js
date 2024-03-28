ec6ee51c7c63dbc18dd3b11d2c4fba4c6cbfd384
hamidrabeei <hamidrabeei@ymail.com>
2024-03-28 19:29:46 +0330

    read products from backend , and added usestate and use effect

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import {React, useState , useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 import Product from '../components/Product/Product'
 
-import products from '../products'
-
 const Home = () => {
+  const [products, setproducts] = useState([])
+
+  useEffect(()=>{
+    const sendRequest = async() =>{
+    const response = await axios.get('http://localhost:8000/api/products')
+    setproducts(response.data)
+
+    }
+
+    sendRequest()
+
+  },[])
+
   return (
     <div>
       <h3 className='text-center'>List products</h3>
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap'
-
-import products from '../products'
+import axios from 'axios'
 
 const Product = ({ match }) => {
-  const product = products.find((item) => {
-    return item._id === match.params.id
-  })
+  const [product, setproducts] = useState({})
+
+  useEffect(()=>{
+
+    const sendRequest = async() =>{
+      const response = await axios.get(`http://localhost:8000/api/products/${match.params.id}`)
+      setproducts(response.data)
+  
+      }
+  
+      sendRequest()
+
+  },[match])
+
 
   return (
     <div>
-     {product.name}
+     <Link to='/' className='btn btn-light my-3'>Back To Home</Link>
+
+     <Row>
+      <Col md={6}>
+        <Image src={product.image} fluid />
+      </Col>
+      <Col md={3}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+              <h3>{product.name}</h3>
+          </ListGroup.Item>
+          <ListGroup.Item >
+              <h5>{product.price}</h5>
+          </ListGroup.Item>
+          <ListGroup.Item>
+              <h5>{product.description}</h5>
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+      <Col md={3}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <Button className='btn-block' type='button'>Add TO Cart😎</Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+     </Row>
     </div>
   )
 }
