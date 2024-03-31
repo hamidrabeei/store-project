6e1b1cb01fff3ebfbe2bb23715a3298cd6999f8d
hamidrabeei <hamidrabeei@ymail.com>
2024-03-31 14:23:01 +0330

    compeleted store file

--- src/store.js
+++ src/store.js
@@ -1,1 +1,12 @@
-import {createStore} from 'redux'
+import {createStore , combineReducers , applyMiddleware} from 'redux'
+import { thunk } from 'redux-thunk' // manage async,await 
+
+ const reducer = combineReducers({ })
+
+ const initialState = {}
+
+ const middleware = {thunk}  // add thunk middleware
+
+const store = createStore(reducer , initialState ,applyMiddleware(...middleware)) //main satore
+
+export default store
